25190a938a1d0965fc94cf16070518c9655584bb
Gemtastic <geminijusa@hotmail.com>
2015-03-08 22:11:33 +0100

    Small editds and adds to the skeleton.

--- src/main/java/model/Appointment.java
+++ src/main/java/model/Appointment.java
@@ -4,5 +4,13 @@ package model;
  * Created by Gemtastic on 2015-03-05.
  */
 public class Appointment {
-
+    private int ID;
+    private String date;
+    private int employeeIDMechanic;
+    private int employeeIDBooker;
+    private int carID;
+    private boolean performed;
+    private int typeID;
+    
+    
 }
---
+++ src/main/java/services/EditEmployeeService.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import services.interfaces.EditingServices;
+
+/**
+ *
+ * @author Gemtastic
+ */
+public class EditEmployeeService implements EditingServices{
+
+    public void edit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Object show() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void delete() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
